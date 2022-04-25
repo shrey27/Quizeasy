75fe52c1ab1ea00b1f19662fdd64ed86f81baa75
Shrey Pandey <shreypandey27@gmail.com>
2022-04-25 18:49:36 +0530

    firebase setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import { db } from './frontend/firebase/firebase';
+import {
+  collection,
+  getDocs
+} from 'firebase/firestore';
+
+const collectionName = 'wishlist';
+const docRef = collection(db, collectionName);
 
 function App() {
+
+  useEffect(() => {
+    (async function () {
+      const data = await getDocs(docRef);
+      const dataList = await data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id
+      }));
+      console.log(dataList);
+    })()
+  })
+
   return (
     <div className="App">
       Quizeazy
@@ -10,3 +30,45 @@ function App() {
 }
 
 export default App;
+/**
+import {
+  collection,
+  doc,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  updateDoc
+} from 'firebase/firestore';
+await addDoc(docRef, objectData);
+await deleteDoc(addrDoc);
+
+const getAddress = async () => {
+    const data = await getDocs(docRef);
+    const dataList = await data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id
+    }));
+    dispatch({ type: 'API_RESPONSE', payload: dataList });
+};
+
+const addNewData = async (payload) => {
+    dispatch({ type: 'API_REQUEST' });
+    await addDoc(docRef, payload);
+    getAddress();
+};
+
+const updateData = async (id, payload) => {
+    dispatch({ type: 'API_REQUEST' });
+    const userDoc = doc(db, collectionName, id);
+    await updateDoc(userDoc, { ...payload });
+    getAddress();
+};
+
+const deleteAddress = async (id) => {
+    dispatch({ type: 'API_REQUEST' });
+    const addrDoc = doc(db, collectionName, id);
+    await deleteDoc(addrDoc);
+    getAddress();
+};
+
+ */
