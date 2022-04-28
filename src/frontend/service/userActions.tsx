6b5c9cbac1330c2c0237ef33969bb29953836e91
Shrey Pandey <shreypandey27@gmail.com>
2022-04-28 17:42:01 +0530

    styling updates

--- src/frontend/service/userActions.tsx
+++ src/frontend/service/userActions.tsx
@@ -14,27 +14,27 @@ export const signInHandler = (email: string, password: string, navigate: Functio
     return async (dispatch: any) => {
         dispatch(userActions.toggleLoader(true));
         const sendUserDetails = async () => {
-            const response = await signInWithEmailAndPassword(auth, email, password)
-            const resUser: any = await response?.user;
-            const { accessToken, uid } = resUser;
-            const docRef = doc(db, userCollection, uid);
-            const docSnap = await getDoc(docRef);
-            dispatch(userActions.getToken(accessToken))
-            dispatch(userActions.getUser(docSnap.data()))
-            dispatch(userActions.toggleLoader(false));
-            localStorage.setItem("authUser", JSON.stringify(docSnap.data()));
-            localStorage.setItem("authToken", JSON.stringify(accessToken));
+            try {
+                const response = await signInWithEmailAndPassword(auth, email, password)
+                const resUser: any = await response?.user;
+                const { accessToken, uid } = resUser;
+                const docRef = doc(db, userCollection, uid);
+                const docSnap = await getDoc(docRef);
+                dispatch(userActions.getToken(accessToken))
+                dispatch(userActions.getUser(docSnap.data()))
+                dispatch(userActions.toggleLoader(false));
+                localStorage.setItem("authUser", JSON.stringify(docSnap.data()));
+                localStorage.setItem("authToken", JSON.stringify(accessToken));
+                ToastMessage('Sign In was Successful', 'success', theme);
+                navigate(pathname);
+            }
+            catch (err) {
+                console.log(err);
+                dispatch(userActions.toggleLoader(false));
+                ToastMessage('Sign In failed! Try Again Later', 'error', theme);
+            };
         }
-        try {
-            sendUserDetails();
-            ToastMessage('Sign In was Successful', 'success', theme);
-            navigate(pathname);
-        }
-        catch (err) {
-            console.log(err);
-            dispatch(userActions.toggleLoader(false));
-            ToastMessage('Sign In failed! Try Again Later', 'error', theme);
-        };
+        sendUserDetails();
     };
 }
 
@@ -42,33 +42,36 @@ export const signUpHandler = (username: string, email: string, password: string,
     return async (dispatch: any) => {
         dispatch(userActions.toggleLoader(true));
         const sendUserDetails = async () => {
-            const response = await createUserWithEmailAndPassword(auth, email, password)
-            const resUser: any = await response?.user;
-            const { accessToken, uid } = resUser;
-            const userObj = {
-                uid,
-                username,
-                email,
-                quiz: [],
-                score: 0
+            try {
+                const response = await createUserWithEmailAndPassword(auth, email, password)
+                const resUser: any = await response?.user;
+                const { accessToken, uid } = resUser;
+                const userObj = {
+                    uid,
+                    username,
+                    email,
+                    quiz: [],
+                    score: 0
+                }
+                await setDoc(doc(db, userCollection, uid), userObj);
+                dispatch(userActions.getToken(accessToken))
+                dispatch(userActions.getUser(userObj))
+                localStorage.setItem("authUser", JSON.stringify(userObj));
+                localStorage.setItem("authToken", JSON.stringify(accessToken));
+                dispatch(userActions.toggleLoader(false));
+                ToastMessage('Sign Up was Successful', 'success', theme);
+                navigate(pathname);
             }
-            await setDoc(doc(db, userCollection, uid), userObj);
-            dispatch(userActions.getToken(accessToken))
-            dispatch(userActions.getUser(userObj))
-            localStorage.setItem("authUser", JSON.stringify(userObj));
-            localStorage.setItem("authToken", JSON.stringify(accessToken));
-            dispatch(userActions.toggleLoader(false));
+            catch (error: any) {
+                switch (error.code) {
+                    case 'auth/email-already-in-use':
+                        dispatch(userActions.toggleLoader(false));
+                        ToastMessage('Email Id is already registered', 'error', theme);
+                        break;
+                }
+            }
         };
-        try {
-            sendUserDetails();
-            navigate(pathname);
-            ToastMessage('Sign Up was Successful', 'success', theme);
-        }
-        catch (error) {
-            console.log(error);
-            dispatch(userActions.toggleLoader(false));
-            ToastMessage('Use different credentials or Try Again Later', 'error', theme);
-        }
+        sendUserDetails();
     };
 };
 
